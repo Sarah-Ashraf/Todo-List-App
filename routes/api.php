8b562f6130a65f341c7caa04660aa73625aa67c8
Sarah_Ashraf <72573298+Sarah-Ashraf@users.noreply.github.com>
2024-07-25 23:51:42 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/tasks', [TaskController::class, 'index']);
+Route::prefix('/task')->group(function () {
+    Route::get('/show', [TaskController::class, 'show']);
+    Route::post('/store', [TaskController::class, 'store']);
+    Route::put('/{id}', [TaskController::class, 'update']);
+    Route::delete('/{task}', [TaskController::class, 'destroy']);
+    Route::patch('/{id}', [TaskController::class, 'restore']);
 });
